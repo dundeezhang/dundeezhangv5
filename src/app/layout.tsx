9067a86695bc3389a1b5c0e4a035bc4674e678df
dundeezhang <dundee_zhang@hotmail.com>
2025-10-14 22:33:55 -0400

    favicon fix?

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,20 +27,6 @@ export const metadata: Metadata = {
   title: "Dundee Zhang",
   description:
     "Dundee Zhang's porfolio. View my projects and other works! Love from Toronto <3.",
-  icons: {
-    icon: [
-      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
-      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
-      { url: "/favicon.ico", sizes: "any" },
-    ],
-    apple: [
-      {
-        url: "/apple-touch-icon.png",
-        sizes: "180x180",
-        type: "image/png",
-      },
-    ],
-  },
   manifest: "/site.webmanifest",
 };
 
