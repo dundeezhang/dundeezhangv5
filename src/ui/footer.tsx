936fab059d48dfd2ec970af5ae938ebec054ed58
dundeezhang <dundee_zhang@hotmail.com>
2025-10-16 14:29:24 -0400

    added bottom margin

--- src/ui/footer.tsx
+++ src/ui/footer.tsx
@@ -7,11 +7,11 @@ export function Footer() {
   const currYear = new Date().getFullYear();
   return (
     <>
-      <footer className="max-w-xl mx-auto my-4 px-4">
+      <footer className="max-w-xl mx-auto mt-4 mb-12 px-4">
         <div className="border-t-2 pt-2 flex justify-between items-center">
           <div className="flex items-center gap-4">
             <ThemeToggle />
-            <p>{currYear} All Rights Reserved.</p>
+            <p>{currYear} Dundee Hu Zhang. All Rights Reserved.</p>
           </div>
           <Webring />
         </div>
